4844d3856e874a0b95b7452abe46196330b29c9f
Strayer1337 <nguyentriet242@gmail.com>
2026-05-13 10:36:09 +0700

    Delete pneumonia_detection_pkg directory

--- pneumonia_detection_pkg/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from pneumonia_detection.data_loader import load_data
-from pneumonia_detection.soft_margin_svm import SoftMarginSVM
-from pneumonia_detection.evaluator import display_results
-
-__all__ = ["load_data", "SoftMarginSVM", "display_results"]
